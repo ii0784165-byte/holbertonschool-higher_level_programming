ad36df00b4ce9a39c8eff37eb4feafbb5c91e337
ii0784165-byte <ii0784165@gmail.com>
2025-11-18 08:17:30 +0000

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if i < j :
-            print("{}{}".format(i, j), end=", " if not (i == 8 and j == 9) else "\n")
+        if i < j:
+            print("{}{}".format(i, j), end=", " if not \
+ (i == 8 and j == 9) else "\n")
