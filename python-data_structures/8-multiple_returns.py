4d296d73c4c85c1b622448d39ebf972569a03be0
ii0784165-byte <ii0784165@gmail.com>
2025-11-23 23:09:09 +0000

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first = sentence[0]
-    if len(sentence) == 0 :
-        first = None
+    if len(sentence) == 0:
+        first_char = None
     else:
-        print("Length: {:d} - First character: {}".format(len(sentence),first))
+        first_char = sentence[0]
+    return (len(sentence), first_char)
+
