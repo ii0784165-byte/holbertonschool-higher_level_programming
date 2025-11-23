8551bc20a37534ef1cb40bacdf7eb42a554277fb
ii0784165-byte <ii0784165@gmail.com>
2025-11-23 23:11:51 +0000

    task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:  # Siyahı boşdursa
+        return None
+    max_val = my_list[0]  # İlk elementi müvəqqəti maksimum kimi götür
+    for num in my_list:
+        if num > max_val:
+            max_val = num
+    return max_val
