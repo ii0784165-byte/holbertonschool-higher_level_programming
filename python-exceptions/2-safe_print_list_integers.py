4ea54279d51787980a0f232c1d29662304aa5339
ii0784165-byte <ii0784165@gmail.com>
2025-11-24 22:10:24 +0000

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    count = 0
     for i in range(x):
         try:
-            print('{:d}'.format(i), end="")
-            return True
-        except (ValueError, IndexError):
-            return False
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+    print()
+    return count
