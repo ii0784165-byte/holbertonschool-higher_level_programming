86146ee55070733fb25474d56450603f86d3712a
ii0784165-byte <ii0784165@gmail.com>
2025-11-24 21:28:43 +0000

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         return None
     best_key = None
     best_value = float('-inf')
-    for key, vlaue in a_dictionary.items():
+    for key, value in a_dictionary.items():
         if value > best_value :
             best_key = key
             best_value = value
