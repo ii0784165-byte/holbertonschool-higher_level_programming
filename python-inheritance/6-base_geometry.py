8757003994be90474ca388c6e8713a02aa8abd27
ii0784165-byte <ii0784165@gmail.com>
2025-11-30 00:47:24 +0000

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message:
+        area() is not implemented
+        """
+        raise Exception("area() is not implemented")
