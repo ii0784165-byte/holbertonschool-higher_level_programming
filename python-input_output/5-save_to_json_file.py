6a706e74e51450488d6839b7f99d56e2aae3559b
ii0784165-byte <ii0784165@gmail.com>
2025-12-01 12:28:33 +0000

    tas k5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     ''' ne poymat i poteryat '''
     with open(filename, "w", encoding="utf-8") as f:
-        json.dumps(my_obj, f)
+        json.dump(my_obj, f)
