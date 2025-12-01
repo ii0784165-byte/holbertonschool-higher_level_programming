150ba78622b9f4923b8ceb2ff0be47a5730f670d
ii0784165-byte <ii0784165@gmail.com>
2025-12-01 10:51:21 +0000

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that contains a function to read and print a text file (UTF8)."""
+
+
 def read_file(filename=""):
-    with open(filename,"r" ,encoding="utf-8") as f:
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
