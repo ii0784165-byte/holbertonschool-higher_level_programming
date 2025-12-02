a28dc943dded014f190edf9abaccab108b608dbd
ii0784165-byte <ii0784165@gmail.com>
2025-12-02 00:32:33 +0000

    task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -18,9 +18,11 @@ class Student:
         Otherwise, all attributes are returned.
         """
         if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
-            return {a: getattr(self, a)
-                    for a in attrs
-                    if hasattr(self, a)}
+            return {
+                a: getattr(self, a)
+                for a in attrs
+                if hasattr(self, a)
+            }
         return self.__dict__
 
     def reload_from_json(self, json):
