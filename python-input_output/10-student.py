3712d876d1653d1fd7010186d3edf0685afb763e
ii0784165-byte <ii0784165@gmail.com>
2025-12-02 00:16:53 +0000

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Student class module"""
+
+
+class Student:
+    """Defines a student with first name, last name, and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only attributes in this list are returned.
+        Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__
