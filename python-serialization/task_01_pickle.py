8912c776689167bb0a1266816ea2d862c44b70d7
ii0784165-byte <ii0784165@gmail.com>
2025-12-02 00:50:59 +0000

    task 1

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,37 @@
+!#/#!/usr/bin/env python3
+"""CustomObject serialization using pickle"""
+
+import pickle
+
+
+class CustomObject:
+    """A custom class with name, age, is_student attributes"""
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Print the object's attributes"""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f'Is Student: {self.is_student}')
+
+    def serialize(self, filename):
+        """Serialize the current instance to a file using pickle"""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize a file to return a CustomObject instance"""
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+            return obj
+        except Exception:
+            return None
