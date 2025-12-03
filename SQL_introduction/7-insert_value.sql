cce9a2b5e9f2e4779c3fe8f9028f72435722f3b1
ii0784165-byte <ii0784165@gmail.com>
2025-12-03 02:54:04 +0000

    task 7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- nserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- Inserts a new row into the table first_table
 INSERT INTO first_table (id, name)
-VALUES (89, 'BEST SCHOOL');
+VALUES (89, 'Best School');
