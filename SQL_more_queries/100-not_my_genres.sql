08ce2618fc06b904fafcef4620b1e8c3df39d994
ii0784165-byte <ii0784165@gmail.com>
2025-12-04 11:40:23 +0000

    task17

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -2,8 +2,9 @@
 SELECT name
 FROM tv_genres
 WHERE id NOT IN (
-    SELECT genre_id
+    SELECT tv_show_genres.genre_id
     FROM tv_show_genres
-    WHERE show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY name ASC;
