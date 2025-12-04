4ce38c6818d59afa097f50e27711ae56abe035d1
ii0784165-byte <ii0784165@gmail.com>
2025-12-04 11:45:19 +0000

    task 19

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
--- asdgdsfg
-SELECT tv_shows.title, SUM(ratings.rating) AS rating
+--hjhfahc
+SELECT tv_shows.title, SUM(tv_shows_rate.rating) AS rating
 FROM tv_shows
-JOIN ratings ON tv_shows.id = ratings.show_id
+JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
 GROUP BY tv_shows.id
 ORDER BY rating DESC;
