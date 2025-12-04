b4832e6c0e42b95c529e2546fc5abfd228189628
ii0784165-byte <ii0784165@gmail.com>
2025-12-04 11:34:23 +0000

    task 14

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,14 @@
--- angsd
-SELECT tv_show_genres.genre_id AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+-- afsagsrgf
+SELECT
+  CASE tv_show_genres.genre_id
+    WHEN 1 THEN 'Drama'
+    WHEN 3 THEN 'Adventure'
+    WHEN 5 THEN 'Comedy'
+    WHEN 7 THEN 'Crime'
+    WHEN 8 THEN 'Suspense'
+    WHEN 9 THEN 'Thriller'
+  END AS genre,
+  COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
