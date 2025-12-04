59183e9557742a676c88e3426e5e136acdf41a29
ii0784165-byte <ii0784165@gmail.com>
2025-12-04 11:24:01 +0000

    task 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- angsdg
+SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.id
+ORDER BY number_of_shows DESC;
