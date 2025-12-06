1ff4e1cce254937c76f36e998f91df9c80115e31
ii0784165-byte <ii0784165@gmail.com>
2025-12-06 22:37:49 +0000

    task 5

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
