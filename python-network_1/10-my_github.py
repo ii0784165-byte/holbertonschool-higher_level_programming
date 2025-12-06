63ecb2cc8b70398d4c3c5bf5a09d7a784bceda04
ii0784165-byte <ii0784165@gmail.com>
2025-12-06 22:58:43 +0000

    task 10

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Python script that uses GitHub API and Basic Auth to get user id"""
 
-import sys
-import requests
+if __name__ == "__main__":
+    import sys
+    import requests
 
-username = sys.argv[1]
-password = sys.argv[2]  # Personal access token
+    username = sys.argv[1]
+    password = sys.argv[2]  # Personal access token
 
-url = "https://api.github.com/user"
+    url = "https://api.github.com/user"
 
-try:
-    response = requests.get(url, auth=(username, password))
-    response_json = response.json()
-    print(response_json.get("id"))
-except requests.RequestException:
-    print(None)
+    try:
+        response = requests.get(url, auth=(username, password))
+        response_json = response.json()
+        print(response_json.get("id"))
+    except requests.RequestException:
+        print(None)
