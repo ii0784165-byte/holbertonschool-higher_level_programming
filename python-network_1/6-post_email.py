0f24db220c424bf38f7e2ad626674c4410245e25
ii0784165-byte <ii0784165@gmail.com>
2025-12-06 22:47:58 +0000

    task 6

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Send a POST request with an email as parameter
+and display the response body.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)
